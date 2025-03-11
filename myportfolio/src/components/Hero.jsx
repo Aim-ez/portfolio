3a79b87a9189b475a986d031684aca7f606ea84c
Aim-ez <154635991+Aim-ez@users.noreply.github.com>
2025-03-11 18:30:02 -0500

    Add framer motion basic element to hero

--- myportfolio/src/components/Hero.jsx
+++ myportfolio/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants'
 import profilePic from '../assets/hero.png'
+import { motion } from 'motion/react'
 
 const Hero = () => {
   return (
@@ -8,7 +9,7 @@ const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className="pb-16 text-2xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Aimee Schmidt</h1>
+                    <motion.h1 animate={{x:100}} className="pb-16 text-2xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Aimee Schmidt</motion.h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">CAV Researcher</span>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
                     <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
