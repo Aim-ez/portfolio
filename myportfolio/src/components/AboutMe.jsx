3f779f3817beec2db433bf11af49d7625471409c
Aim-ez <154635991+Aim-ez@users.noreply.github.com>
2025-03-13 18:06:39 -0500

    Add animation to AboutMe section

--- myportfolio/src/components/AboutMe.jsx
+++ myportfolio/src/components/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import aboutImg from '../assets/eyeball.png'
 import { ABOUT_TEXT } from '../constants'
+import { motion } from 'motion/react'
 
 const AboutMe = () => {
   return (
@@ -8,16 +9,24 @@ const AboutMe = () => {
         <h1 className='text-4xl my-20 text-center'>About Me</h1>
 
         <div className='flex flex-wrap'>
-            <div className='w-full lg:w-1/2 lg:p-8'>
+            <motion.div 
+                whileInView={{opacity: 1, x: 0}}
+                initial={{opacity: 0, x:-100}}
+                transition={{duration: 0.5}}
+                className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex items-center justify-center'>
                     <img className="rounded-2xl" src={aboutImg} alt="about" />
                 </div>
-            </div>
-            <div className='w-full lg:w-1/2'>
+            </motion.div>
+            <motion.div 
+                whileInView={{opacity: 1, x: 0}}
+                initial={{opacity: 0, x:100}}
+                transition={{duration: 0.5}}
+                className='w-full lg:w-1/2'>
                 <div className='flex justify-center lg:justify-start'>
                     <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{ABOUT_TEXT}</p>
                 </div>
-            </div>
+            </motion.div>
         </div>
 
 
