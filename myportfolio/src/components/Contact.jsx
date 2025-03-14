388508ad941e1097bb7915d7735633f32b8439cf
Aim-ez <154635991+Aim-ez@users.noreply.github.com>
2025-03-13 19:06:39 -0500

    Add animation to Contact section

--- myportfolio/src/components/Contact.jsx
+++ myportfolio/src/components/Contact.jsx
@@ -1,14 +1,24 @@
 import { CONTACT } from "../constants"
+import { motion } from "motion/react"
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl">Contact Me</h1>
-        <div className="text-center tracking-tighter">
+        <motion.h1 
+          whileInView={{opacity: 1, y: 0}}
+          initial={{opacity: 0, y: 100}}
+          transition={{duration: 1}}
+          className="my-20 text-center text-4xl">Contact Me
+        </motion.h1>
+        <motion.div 
+          whileInView={{opacity: 1}}
+          initial={{opacity: 0}}
+          transition={{duration: 3}}
+          className="text-center tracking-tighter">
             <p className="my-4">{CONTACT.address}</p>
             <p className="my-4">{CONTACT.phoneNo}</p>
             <p className="my-4">{CONTACT.email}</p>
-        </div>
+        </motion.div>
     </div>
   )
 }
